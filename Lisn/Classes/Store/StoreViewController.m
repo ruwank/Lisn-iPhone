7436454acde76bcb9bc20cd17b73a64841f44778
Amsureshs <sureshsumanasooriya@gmail.com>
2016-03-30 10:21:34 +0530

    store view

--- Lisn/Classes/Store/StoreViewController.m
+++ Lisn/Classes/Store/StoreViewController.m
@@ -7,12 +7,60 @@
 //
 
 #import "StoreViewController.h"
+#import "TopTabsScrollView.h"
+#import "AppConstant.h"
 
 @interface StoreViewController ()
+
+@property (weak, nonatomic) IBOutlet TopTabsScrollView *topTabScrollView;
+@property (weak, nonatomic) IBOutlet UIView *tabSelect1;
+@property (weak, nonatomic) IBOutlet UIView *tabSelect2;
+@property (weak, nonatomic) IBOutlet UIView *tabSelect3;
+@property (weak, nonatomic) IBOutlet UIView *tabSelect4;
+@property (weak, nonatomic) IBOutlet UIView *tabSelect5;
+@property (weak, nonatomic) IBOutlet UIView *tabSelect6;
+
+
 
 @end
 
 @implementation StoreViewController
+
+- (IBAction)tabButtonTapped:(id)sender {
+    UIButton *btn = (UIButton *)sender;
+    
+    _tabSelect1.backgroundColor = RGBA(238, 159, 31, 1);
+    _tabSelect2.backgroundColor = RGBA(238, 159, 31, 1);
+    _tabSelect3.backgroundColor = RGBA(238, 159, 31, 1);
+    _tabSelect4.backgroundColor = RGBA(238, 159, 31, 1);
+    _tabSelect5.backgroundColor = RGBA(238, 159, 31, 1);
+    _tabSelect6.backgroundColor = RGBA(238, 159, 31, 1);
+    
+    switch (btn.tag) {
+        case 1:
+            _tabSelect1.backgroundColor = RGBA(255, 255, 255, 1);
+            break;
+        case 2:
+            _tabSelect2.backgroundColor = RGBA(255, 255, 255, 1);
+            break;
+        case 3:
+            _tabSelect3.backgroundColor = RGBA(255, 255, 255, 1);
+            break;
+        case 4:
+            _tabSelect4.backgroundColor = RGBA(255, 255, 255, 1);
+            break;
+        case 5:
+            _tabSelect5.backgroundColor = RGBA(255, 255, 255, 1);
+            break;
+        case 6:
+            _tabSelect6.backgroundColor = RGBA(255, 255, 255, 1);
+            break;
+            
+        default:
+            break;
+    }
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
