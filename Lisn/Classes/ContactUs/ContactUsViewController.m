52fcb4ea1777f67eda6d9ff4c8b6c213ee9d42d8
Amsureshs <sureshsumanasooriya@gmail.com>
2016-05-08 17:33:11 +0530

    other screens

--- Lisn/Classes/ContactUs/ContactUsViewController.m
+++ Lisn/Classes/ContactUs/ContactUsViewController.m
@@ -7,16 +7,28 @@
 //
 
 #import "ContactUsViewController.h"
+#import "AppConstant.h"
+#import <MessageUI/MessageUI.h>
 
-@interface ContactUsViewController ()
+@interface ContactUsViewController () <MFMailComposeViewControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
 
 @end
 
 @implementation ContactUsViewController
 
+- (IBAction)emailButtonTapped:(id)sender {
+    [self sendMail];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"contactus" ofType:@"html"]];
+    [_webView loadRequest:[NSURLRequest requestWithURL:url]];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +36,31 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)sendMail
+{
+    if (![MFMailComposeViewController canSendMail]) {
+        NSLog(@"Mail services are not available.");
+        return;
+    }
+    
+    MFMailComposeViewController* composeVC = [[MFMailComposeViewController alloc] init];
+    composeVC.mailComposeDelegate = self;
+    
+    // Configure the fields of the interface.
+    [composeVC setToRecipients:@[CONTACT_US_MAIL]];
+    
+    // Present the view controller modally.
+    [self presentViewController:composeVC animated:YES completion:nil];
 }
-*/
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller
+          didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    // Check the result or perform other tasks.
+    
+    // Dismiss the mail compose view controller.
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
 
 @end
